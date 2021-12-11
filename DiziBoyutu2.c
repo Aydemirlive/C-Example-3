50f8e108d05fba65648b700d8a779e16444c2db5
Aydemirlive <95748228+Aydemirlive@users.noreply.github.com>
2021-12-11 10:17:46 +0300

    Add files via upload

---
+++ DiziBoyutu2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char metin[61];
+	int i;
+	printf("bir metin girin\n");
+	scanf("%s",metin);
+	for(i=0;metin[i]!='\0';i++){
+		
+	}printf("%d",i);
+	
+	return 0;
+}
